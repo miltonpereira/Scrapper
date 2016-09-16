819c775c35dfda4fd70f98860d2d19baeadbc32d
Milton <milton@sjinnovation.com>
2016-09-16 18:58:58 +0530

    Script is running with new sturcture

--- main_script_file.py
+++ main_script_file.py
@@ -12,11 +12,12 @@ def GenerateHtml(base_url,payload):
 
 def parsing_and_scrape_data(raw_data):
 		
-		product_details={}
+		
 		soup = BeautifulSoup(raw_data,'html.parser')
 		g_data= (soup.find_all('div',{"class":"g"}))
 		
 		for items in g_data:
+			product_details={}
 			atag= items.a
 			cite= items.cite
 			title = (atag.text)
@@ -24,18 +25,18 @@ def parsing_and_scrape_data(raw_data):
 			print (link)
 			product_details["Title"]=title
 			product_details["Links"]=link
+			write_on_csv(product_details)
 		
-		return product_details
 		
-		write_on_csv(data)
+
 
 def write_on_csv(data):
 
 	file_exists = os.path.isfile('googleresults.csv')
-
-	with open ('googleresults.csv', 'w',newline='') as csvfile:
+	with open ('googleresults.csv', 'a',newline='') as csvfile:
 		fieldnames = ['Title', 'Links']
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		
 		if not file_exists:
 			writer.writeheader()
 		
--- modified_googlescript.py
+++ modified_googlescript.py
@@ -2,29 +2,24 @@ import requests
 import time 
 import main_script_file 
 
-pg_no=0
-base_url="https://www.google.co.in/search"
-keyword=('QA team') # Kewyword to scrap data for
-payload = {'q': keyword, 'start': pg_no}
 
-data=main_script_file.GenerateHtml(base_url,payload)
-final_data=main_script_file.parsing_and_scrape_data(data)
-#main_script_file.write_on_csv(final_data)
-
-"""pg_no=0 
+pg_no=0 
 total_pages=10 #This number of pages it will scrap data
 
 while pg_no<=total_pages: 
 	base_url="https://www.google.co.in/search"
-	keyword=('where am i') # Kewyword to scrap data for
+	keyword=('QA Companies') # Kewyword to scrap data for
 	payload = {'q': keyword, 'start': pg_no}
 	#headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
 	
-	raw_data=main_script_file.raw_data_generate_html_from_provided_url(base_url,payload)
-	print(raw_data)
+	data=main_script_file.GenerateHtml(base_url,payload)
+	final_data=main_script_file.parsing_and_scrape_data(data)
+	
 
 	pg_no=10+pg_no
 	time.sleep(60)
-"""
+
+print ('Your script has been executed! Bye')
+
 
 	
